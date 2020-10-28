95c07ad88636a5298ed165659810ebdb124b8428
svrnn <73329995+svrnn@users.noreply.github.com>
2020-10-28 14:05:49 +0700

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,12 @@
+import pyautogui, time
+time.sleep(5)
+f = open("spammertext", 'r')
+for word in f:
+    pyautogui.typewrite(word)
+    pyautogui.press("enter")
+    
+    #Made by svrn#1098
+    #You can use this on Instagram, Whatsapp and etc!
+    #Simple python 5 lines of code spammer.
+    #Any problem? dm svrn#1098
+    #Make sure you are using PyCharm Edu!
