e28cc2c2251a73ec9c394ab39d63133573c9ce31
Alllter <73329995+Alllter@users.noreply.github.com>
2020-10-29 09:43:56 +0700

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -5,8 +5,8 @@ for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
     
-    #Made by svrn#1098
+    #Made by Alter#1098
     #You can use this on Instagram, Whatsapp and etc!
     #Simple python 5 lines of code spammer.
-    #Any problem? dm svrn#1098
+    #Any problem? dm Alter#1098
     #Make sure you are using PyCharm Edu!
